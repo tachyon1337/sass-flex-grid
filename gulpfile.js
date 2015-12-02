1050f9f4dc4c75d63308268e15ce779daae61889
Scott <sfrancis@misinteractive.com>
2015-12-01 22:37:42 -0500

    Added bower.json

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 
 var gulp=require('gulp');
 var sassdoc = require('sassdoc');
+var sass = require('gulp-sass');
+var REPO_NAME='sass-flex-grid';
 
 gulp.task('default',function(){
-    console.log('echo sass flex-grid...');
+    console.log(REPO_NAME + ' ..."tasks: gulp dist|sassdoc"');
 });
 
 gulp.task('sassdoc', function () {
@@ -11,3 +13,16 @@ gulp.task('sassdoc', function () {
         .pipe(sassdoc());
 
 });
+
+gulp.task('dist', function () {
+    compileSass();
+});
+
+
+///private
+function compileSass(){
+    gulp.src('./src/flex-grid.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('./dist'));
+}
+
