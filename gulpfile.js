17c70ac1751b08f9a9a91c2eeef6fa6bafe39dd8
Scott <sfrancis@misinteractive.com>
2016-02-28 12:13:33 -0500

    Minor version update

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,9 @@
 var gulp=require('gulp');
 var sassdoc = require('sassdoc');
 var sass = require('gulp-sass');
+var concat=require('gulp-concat');
+var uglify = require('gulp-uglify');
+var minify = require('gulp-minify-css');
 var REPO_NAME='sass-flex-grid';
 
 gulp.task('default',function(){
@@ -18,11 +21,22 @@ gulp.task('dist', function () {
     compileSass();
 });
 
+gulp.task('minify',function(){
+    concatStream('./dist/flex.grid.css','flex.grid.min.css')
+        .pipe(minify())
+        .pipe(gulp.dest('./dist'));
+});
+
 
 ///private
 function compileSass(){
-    gulp.src('./src/flex-grid.scss')
+    gulp.src('./src/flex.grid.scss')
         .pipe(sass())
         .pipe(gulp.dest('./dist'));
 }
 
+function concatStream(src,name){
+    return gulp.src(src)
+        .pipe(concat(name))
+
+}
